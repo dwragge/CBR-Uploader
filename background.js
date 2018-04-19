38cc9d9cc09117571339e9c67775eed4e3f17d43
Dylan Wragge <dylanwragge@gmail.com>
2018-04-19 16:25:42 +1200

    add pdfjs to extension, update for new invoice format and should add NC* support

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
+function loadScript(scriptName, callback) {
+    var scriptEl = document.createElement('script');
+    scriptEl.src = chrome.extension.getURL(scriptName + '.js');
+    scriptEl.addEventListener('load', callback, false);
+    document.head.appendChild(scriptEl);
+}
+
+loadScript('pdf')
+loadScript('pdf.worker')
+
 var s = document.createElement('script');
 // TODO: add "script.js" to web_accessible_resources in manifest.json
 s.src = chrome.extension.getURL('popup.js');
@@ -5,3 +15,4 @@ s.onload = function() {
     this.remove();
 };
 (document.head || document.documentElement).appendChild(s);
+
--- popup.js
+++ popup.js
@@ -1,5 +1,4 @@
 (function() {
-
 var reloaded = false
 $(document).ready(function() {
     var invoice = JSON.parse(window.localStorage.getItem("invoice"))
@@ -33,166 +32,162 @@ $(document).ready(function() {
 })
 
 if (reloaded === true) exit()
-
-$.getScript('http://mozilla.github.io/pdf.js/build/pdf.js', function() {
-    PDFJS.workerSrc = 'http://mozilla.github.io/pdf.js/build/pdf.worker.js';
-    function loadPdf(data) {
-        return PDFJS.getDocument({data: data}).then(function(pdf) {
-            var pages = [];
-            for (var i = 0; i < pdf.numPages; i++) {
-                pages.push(i);
-            }
-            return Promise.all(pages.map(function(pageNumber) {
-                return pdf.getPage(pageNumber + 1).then(function(page) {
-                    return page.getTextContent().then(function(textContent) {
-                        return textContent.items.map(function(item) {
-                            return item.str;
-                        }).join(' ');
-                    });
+function loadPdf(data) {
+    return PDFJS.getDocument({data: data}).then(function(pdf) {
+        var pages = [];
+        for (var i = 0; i < pdf.numPages; i++) {
+            pages.push(i);
+        }
+        return Promise.all(pages.map(function(pageNumber) {
+            return pdf.getPage(pageNumber + 1).then(function(page) {
+                return page.getTextContent().then(function(textContent) {
+                    return textContent.items.map(function(item) {
+                        return item.str;
+                    }).join(' ');
                 });
-            })).then(function(pages) {
-                return pages.join("\r\n");
             });
+        })).then(function(pages) {
+            return pages.join("\r\n");
+        });
+    });
+}
+
+function cleanText(text) {
+    startIndex = text.indexOf("Amount$");
+    endIndex =  text.indexOf("Total Discount");
+    if (endIndex === -1) {
+        endIndex = text.indexOf("Total Sale")
+    }
+    text = text.substring(startIndex, endIndex)
+    text = text.replace(/\s+/g,' ').trim();
+    return text;
+}
+
+function parseDiscounts(discountsText) {
+    if (discountsText === undefined) {
+        return 0.0
+    }
+
+    var totalDiscountValue = 0.0
+    var regex = /[0-9.]+/g
+    var match = regex.exec(discountsText)
+    while (match != null) {
+        var value = parsePrice(match[0])
+        totalDiscountValue += value
+        match = regex.exec(discountsText)
+    }
+    return totalDiscountValue
+}
+
+function parsePrice(priceString) {
+    priceString = priceString.trim()
+    priceString = priceString.replace(',', '')
+    return parseFloat(priceString)
+}
+
+function Product(modelNumber, quantity, basePrice, discount) {
+    this.name = products[modelNumber]
+    this.modelNumber = modelNumber
+    this.quantity = quantity
+    if (discount > basePrice) {
+        throw new Error("Discount was greater than sell price!")
+    }
+    this.sellPrice = (quantity * basePrice) - discount
+}
+
+function parseProdcuts(pdfText) {
+    var text = pdfText;
+    var regex = /(?:\d Years|\d+ Months) \d{4,6} (?:NC\*\s)?Fisher & Paykel [^\(\)]+\(([A-Z]{1,2}\d{2,4}[A-Z]*\d?)\) (\d) ([0-9.,]+) 15 [0-9.,]+ (Discount (?:-[0-9.,]+\s)+)*/g
+    var match = regex.exec(text)
+    var products = []
+    while (match != null) {
+        var modelNumber = match[1]
+        var quantity = parseInt(match[2])
+        var basePrice = parsePrice(match[3])
+        var discount = parseDiscounts(match[4])
+
+        products.push(new Product(modelNumber, quantity, basePrice, discount))
+        match = regex.exec(text)
+    }
+    return products
+}
+
+function getName(text) {
+    var regex = /Cust\. no\.: \d+ ((?:[A-Za-z -]+)+)/
+    var match = regex.exec(text)
+    var name = match[1].trim()
+    if (name === "Latitude Financial Services") {
+        var regex1 = /\|\s+((?:\w\s?)+)/
+        var match1 = regex1.exec(text)
+        name = match1[1].trim()
+    }
+    return name
+}
+
+function getDate(text) {
+    var regex = /Date [0-9]{9}\s+((?:[0-9]{1,4}\/?){3})/
+    var match = regex.exec(text)
+    return match[1]
+}
+
+function getInvNo(text) {
+    var regex = /Date ([0-9]{9})/
+    var match = regex.exec(text)
+    return match[1].trim()
+}
+
+// Get all products into array
+var products = {}
+$("#ctl00_cpMain_ddModel option").each(function() { 
+    products[$(this).val()] = $(this).text() 
+})
+
+function fillForm() {
+    var invoice = JSON.parse(window.localStorage.getItem("invoice"))
+
+    $('#ctl00_cpMain_ddModelundefined').val(invoice.name)
+    $('#ctl00_cpMain_ddModel').val(invoice.products[0].modelNumber)
+    $('#ctl00_cpMain_ddModel').data('combobox').refresh()
+    $find("ctl00_cpMain_dtEarned_dateInput").set_value(invoice.date)
+    $('#ctl00_cpMain_txtInvoice').val(invoice.invoiceNumber)
+    $('#ctl00_cpMain_txtCustomerName').val(invoice.name)
+    $('#ctl00_cpMain_txtUnits').val(invoice.products[0].quantity)
+    $('#ctl00_cpMain_txtPrice').val(invoice.products[0].sellPrice)
+    
+    invoice.products.shift()
+    window.localStorage.setItem("invoice", JSON.stringify(invoice))
+
+    //document.getElementById('ctl00_cpMain_btnSubmit').click()
+}
+
+function addAutoFillButton() {
+    var button = $('<input type="button" value="Auto Add Invoice" />')
+    button.click(fillForm)
+    $('#ctl00_cpMain_AsyUpDocumentrow1').append(button)
+}
+
+// How to add a listener to the files uploaded and get a ref to it
+var handleFiles = function() {
+    console.log(this.files)
+    var reader = new FileReader()
+    reader.onload = function() {
+        loadPdf(reader.result).then(function(text) {
+            console.log(text)
+            var products = parseProdcuts(text)
+            var invoice = {
+                "invoiceNumber": getInvNo(text),
+                "date": getDate(text),
+                "name": getName(text),
+                "products": parseProdcuts(text)
+            }
+            console.log(invoice)
+            window.localStorage.setItem("invoice", JSON.stringify(invoice))
+            addAutoFillButton()
         });
     }
-    
-    function cleanText(text) {
-        startIndex = text.indexOf("Amount$");
-        endIndex =  text.indexOf("Total Discount");
-        if (endIndex === -1) {
-            endIndex = text.indexOf("Total Sale")
-        }
-        text = text.substring(startIndex + 91, endIndex)
-        text = text.replace(/\s+/g,' ').trim();
-        return text;
-    }
-    
-    function parseDiscounts(discountsText) {
-        discountsText = discountsText.trim()
-        var totalDiscountValue = 0.0
-        var regex = /[0-9.]+/g
-        var match = regex.exec(discountsText)
-        while (match != null) {
-            var value = parsePrice(match[0])
-            totalDiscountValue += value
-            match = regex.exec(discountsText)
-        }
-        return totalDiscountValue
-    }
-    
-    function parsePrice(priceString) {
-        priceString = priceString.trim()
-        priceString = priceString.replace(',', '')
-        return parseFloat(priceString)
-    }
-    
-    function Product(modelNumber, quantity, basePrice, discount) {
-        this.name = products[modelNumber]
-        this.modelNumber = modelNumber
-        this.quantity = quantity
-        if (discount > basePrice) {
-            throw new Error("Discount was greater than sell price!")
-        }
-        this.sellPrice = quantity * (basePrice - discount)
-    }
-    
-    function parseProdcuts(pdfText) {
-        var text = cleanText(pdfText);
-        var regex = /(?:\d YRS)?\s\d+\sF&P\s(\w+).+?(?=\d\s@)(\d)\s@\s([0-9,.]+)\s[0-9.]+\s[0-9.,]+((?:\sDiscount : [0-9.,]+-)*)/g
-        var match = regex.exec(text)
-        var products = []
-        while (match != null) {
-            var modelNumber = match[1]
-            var quantity = parseInt(match[2])
-            var basePrice = parsePrice(match[3])
-            var discount = parseDiscounts(match[4])
-    
-            products.push(new Product(modelNumber, quantity, basePrice, discount))
-            match = regex.exec(text)
-        }
-        return products
-    }
-    
-    function getName(text) {
-        var regex = /Name: \s+((?:\w+\s?)+)/
-        var match = regex.exec(text)
-        var name = match[1].trim()
-        if (name === "Latitude Financial Services") {
-            var regex1 = /\|\s+((?:\w\s?)+)/
-            var match1 = regex1.exec(text)
-            name = match1[1].trim()
-        }
-        return name
-    }
-    
-    function getDate(text) {
-        var regex = /Date:\s+((?:[0-9]{1,2}\/?){3})/
-        var match = regex.exec(text)
-        var dateSplit = match[1].split('/')
-        if (dateSplit[0].startsWith('0')) {
-            dateSplit[0] = dateSplit[0].substr(1)
-        }
-        dateSplit[2] = '20'+ dateSplit[2]
-        return dateSplit.join('/')
-    }
-    
-    function getInvNo(text) {
-        var regex = /Inv. no.:\s+(\d+)/
-        var match = regex.exec(text)
-        return match[1].trim()
-    }
-    
-    // Get all products into array
-    var products = {}
-    $("#ctl00_cpMain_ddModel option").each(function() { 
-        products[$(this).val()] = $(this).text() 
-    })
-    
-    function fillForm() {
-        var invoice = JSON.parse(window.localStorage.getItem("invoice"))
-
-        $('#ctl00_cpMain_ddModelundefined').val(invoice.name)
-        $('#ctl00_cpMain_ddModel').val(invoice.products[0].modelNumber)
-        $('#ctl00_cpMain_ddModel').data('combobox').refresh()
-        $find("ctl00_cpMain_dtEarned_dateInput").set_value(invoice.date)
-        $('#ctl00_cpMain_txtInvoice').val(invoice.invoiceNumber)
-        $('#ctl00_cpMain_txtCustomerName').val(invoice.name)
-        $('#ctl00_cpMain_txtUnits').val(invoice.products[0].quantity)
-        $('#ctl00_cpMain_txtPrice').val(invoice.products[0].sellPrice)
-        
-        invoice.products.shift()
-        window.localStorage.setItem("invoice", JSON.stringify(invoice))
-
-        document.getElementById('ctl00_cpMain_btnSubmit').click()
-    }
-
-    function addAutoFillButton() {
-        var button = $('<input type="button" value="Auto Add Invoice" />')
-        button.click(fillForm)
-        $('#ctl00_cpMain_AsyUpDocumentrow1').append(button)
-    }
-
-    // How to add a listener to the files uploaded and get a ref to it
-    var handleFiles = function() {
-        console.log(this.files)
-        var reader = new FileReader()
-        reader.onload = function() {
-            loadPdf(reader.result).then(function(text) {
-                var products = parseProdcuts(text)
-                var invoice = {
-                    "invoiceNumber": getInvNo(text),
-                    "date": getDate(text),
-                    "name": getName(text),
-                    "products": parseProdcuts(text)
-                }
-                console.log(invoice)
-                window.localStorage.setItem("invoice", JSON.stringify(invoice))
-                addAutoFillButton()
-            });
-        }
-        reader.readAsBinaryString(this.files[0])
-    }
-    var inputElement = $('*[id^="ctl00_cpMain_AsyUpDocumentfile"]')[0]
-    inputElement.addEventListener("change", handleFiles, false)
-})
+    reader.readAsBinaryString(this.files[0])
+}
+var inputElement = $('*[id^="ctl00_cpMain_AsyUpDocumentfile"]')[0]
+if (inputElement === undefined) return
+inputElement.addEventListener("change", handleFiles, false)
 })()
